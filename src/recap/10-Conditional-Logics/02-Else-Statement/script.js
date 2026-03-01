f479ff5650e26dcf60ba65d44222a75d5cec2357
karamo10 <pabimamie@gmail.com>
2026-03-01 12:04:38 +0000

    feat: Add else statement example with conditional logic

--- src/recap/10-Conditional-Logics/02-Else-Statement/script.js
+++ src/recap/10-Conditional-Logics/02-Else-Statement/script.js
@@ -1,8 +1,19 @@
 // else statement
 
 // when a 'if statement' is true the code block right after the curly braces wil run to true and if not true nothing happens
-// but with an else statement and altimate block of code can be executed when it is not true 
+// but with an else statement and altimate block of code can be executed when it is not true
+
+function testElse(val) {
+  if (val > 20) {
+    result = 'Greather than 20';
+  } else {
+    result = 'Less than 20';
+  }
+  return result;
+}
+
+console.log(testElse(35)); // Greather than 20
 
 
 
-// node src/recap/10-Conditional-Logics/02-Else-Statement/script.js 
+// node src/recap/10-Conditional-Logics/02-Else-Statement/script.js
