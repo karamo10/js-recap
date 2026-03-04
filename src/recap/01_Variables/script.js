8c67e0a7f6b024043ca06addea00352cf99a1399
karamo10 <pabimamie@gmail.com>
2026-03-04 01:07:08 +0000

    fix: add console log for today's task in script.js

--- src/recap/01_Variables/script.js
+++ src/recap/01_Variables/script.js
@@ -39,5 +39,6 @@ my_variable = "JavaScript";
 
 // but it's good practice to use camelCase for variable names in JavaScript
 
+console.log("That's todays task!");
 
 // node src/recap/01_Variables/script.js
